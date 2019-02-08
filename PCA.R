e07f02a22fc3268dbf76554373f42151791db597
JosephPablo <31300061+JosephPablo@users.noreply.github.com>
2019-02-08 20:58:47 +0800

    PCA for dimension reduction

---
+++ PCA.R
@@ -0,0 +1,38 @@
+#Principal Component Analysis And Factor Analysis 
+#Loading Data from the local disk
+mydata<- read.csv("/Users/varun/Desktop/git/pca_gsp.csv")
+attach(mydata)
+# Define variables
+X <- cbind(Ag, Mining, Constr, Manuf, Manuf_nd, Transp, Comm, Energy, TradeW, TradeR,
+           RE, Services, Govt)
+
+# Descriptive statistics
+summary(X)
+cor(X)
+
+# Principal component analysis
+pca1 <- princomp(X, scores=TRUE, cor=TRUE)
+summary(pca1)
+
+# Loadings of principal components
+loadings(pca1)
+#pca1$loadings
+
+# Scree plot of eigenvalues
+plot(pca1)
+screeplot(pca1, type="line", main="Scree Plot")
+# Biplot of score variables
+biplot(pca1)
+# Scores of the components
+pca1$scores[1:10,]
+
+# Rotation
+#varimax(pca1$rotation)
+#promax(pca1$rotation)
+# Factor analysis - different results from other softwares and no rotation
+fa1 <- factanal(X, factor=3)
+fa1
+fa2 <- factanal(X, factor=3, rotation="varimax")
+fa2
+fa3 <- factanal(X, factors=3, rotation="varimax", scores="regression")
+fa3
